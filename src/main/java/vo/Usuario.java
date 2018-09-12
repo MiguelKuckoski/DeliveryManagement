371cf8561e25b180bd160578e3113b17cae3e59f
Miguel Kuckoski <miguelfreitas32@gmail.com>
2018-09-11 21:10:02 -0300

    IMPROVEMENT: iniciado criação dos métodos de usuário.

--- src/main/java/vo/Usuario.java
+++ src/main/java/vo/Usuario.java
@@ -1,24 +1,48 @@
 package vo;
 
 public class Usuario {
-	public void cadastrarObjeto() {
-		
+	private Controle controle;
+
+	public Usuario(Controle controle) {
+		this.controle = controle;
 	}
-	
+
+	public void cadastrarObjeto(String nomeRemetente, Endereco endRemetente, String nomeDestinatario,
+			Endereco endDestinatario, Double peso, String codLocalizador) {
+
+		Pacote pacote = new Pacote(nomeRemetente, endRemetente, nomeDestinatario, endDestinatario, peso,
+				codLocalizador);
+		controle.addPacote(pacote);
+	}
+
 	public void criarRoteiro() {
-		
+		// TODO
 	}
-	
+
 	public void listarRoteiro() {
-		
+		// TODO
 	}
-	
-	public void cadastrarVeiculo() {
-		
+
+	public void cadastrarVeiculo(String marca, String modelo, int ano, String placa, String tipo, Motorista motorista) {
+		Veiculo veiculo = new Veiculo();
+		if (tipo.equalsIgnoreCase("van")) {
+			veiculo = new Van(marca, modelo, ano, placa, tipo);
+		} else if (tipo.equalsIgnoreCase("caminhão")) {
+			veiculo = new Caminhao(marca, modelo, ano, placa, tipo);
+		} else if (tipo.equalsIgnoreCase("carreta")) {
+			veiculo = new Carreta(marca, modelo, ano, placa, tipo);
+		}
+
+		if (motorista != null) {
+			veiculo.setMotorista(motorista);
+		}
+
+		controle.addVeiculo(veiculo);
 	}
-	
-	public void cadastrarMotorista() {
-		
+
+	public void cadastrarMotorista(String nome, String nascimento, Endereco endereco, String cnhNum, String cnhTipo) {
+		Motorista motorista = new Motorista(nome, nascimento, endereco, cnhNum, cnhTipo);
+		controle.addMotorista(motorista);
 	}
 
 }
