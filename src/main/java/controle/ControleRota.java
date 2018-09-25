870c4b9fb1925b423acd432a6b6ea7c5823938bd
Miguel Kuckoski <miguelfreitas32@gmail.com>
2018-09-25 20:36:46 -0300

    Criado filtro de busca de rotas

--- src/main/java/controle/ControleRota.java
+++ src/main/java/controle/ControleRota.java
@@ -15,6 +15,8 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.TreeSet;
 
+import javax.swing.JOptionPane;
+
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
@@ -35,7 +37,7 @@ public class ControleRota {
 	static final String PATH = "C:/rotas/";
 	final SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
 	final SimpleDateFormat fileDateFormat = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
-	
+
 	private ControladorPrincipal controlePrincipal;
 
 	public ControleRota(ControladorPrincipal controladorPrincipal) {
@@ -130,7 +132,6 @@ public class ControleRota {
 			/******** Open File ********/
 			try {
 				workBook = WorkbookFactory.create(file);
-
 			} catch (EncryptedDocumentException e1) {
 				e1.printStackTrace();
 			} catch (InvalidFormatException e1) {
@@ -154,7 +155,8 @@ public class ControleRota {
 
 					Pacote pacote = new Pacote();
 					try {
-						pacote.setDataInsercao(fileDateFormat.parse(row.getCell(FileConstants.DATA_INSERCAO).getStringCellValue()));
+						pacote.setDataInsercao(
+								fileDateFormat.parse(row.getCell(FileConstants.DATA_INSERCAO).getStringCellValue()));
 					} catch (ParseException e) {
 						System.err.println(e.getMessage());
 					}
@@ -204,7 +206,7 @@ public class ControleRota {
 			});
 		});
 	}
-	
+
 	public void autoSizeColumns(Workbook workbook) {
 		int numberOfSheets = workbook.getNumberOfSheets();
 		for (int i = 0; i < numberOfSheets; i++) {
@@ -267,11 +269,112 @@ public class ControleRota {
 	}
 
 	public void foraDaRoteirizacaoDiaria() {
+
 		controlePrincipal.getControlePacote().getPacoteDAO().getListaPacote().entrySet().forEach(pacote -> {
 			if (!pacote.getValue().isRoteirizado() && !pacote.getValue().isEntrega()) {
 				System.out.println("rastreio: " + pacote.getValue().getCodLocalizador() + "\n");
 			}
 		});
+
 	}
 
+	
+	public void pesquisaDataMotorista(String motorista, String data)
+			throws EncryptedDocumentException, InvalidFormatException, IOException {
+		File searchFile = null;
+		File[] files = null;
+		Workbook workBook = null;
+		String [] fileName = null;
+		if (data.isEmpty()) {
+			searchFile = new File(PATH);
+			files = searchFile.listFiles();
+
+			for (File file : files) {
+				if (file.exists()) {
+					fileName = file.getPath().split("/");
+					workBook = WorkbookFactory.create(file);
+					System.out.println("--- Rota: " + fileName[fileName.length -1]+ " ---");
+					openFile(workBook, motorista);
+				}
+			}
+
+		} else {
+			searchFile = new File(data + "-rota.xls");
+			if (searchFile.exists()) {
+				workBook = WorkbookFactory.create(searchFile);
+				fileName = searchFile.getPath().split("/");
+				System.out.println("--- Rota: " + fileName[fileName.length -1]+ " ---");
+				openFile(workBook, motorista);
+
+			} else {
+				JOptionPane.showMessageDialog(null, "Rota inexistente pra data informada.");
+			}
+		}
+
+	}
+
+	
+	private void openFile(Workbook workBook, String motorista) {
+		Sheet sheet = null;
+		if (!motorista.isEmpty()) {
+			sheet = workBook.getSheet(motorista);
+
+			if (sheet != null) {
+				readFile(sheet);
+			} else {
+				JOptionPane.showMessageDialog(null, "O motorista " + motorista + "não possui rotas na data" + "");
+			}
+		} else {
+			for (int i = 0; i < workBook.getNumberOfSheets(); i++) {
+				sheet = workBook.getSheetAt(i);
+			}
+
+		}
+
+	}
+
+	private void readFile(Sheet sheet) {
+		Iterator<Row> rowIterator = sheet.iterator();
+		Row row = null;
+		Veiculo veiculo = null;
+		Motorista motorista = null;
+
+		while (rowIterator.hasNext()) {
+			row = rowIterator.next();
+			if (row.getRowNum() == 0) {
+				continue;
+			}
+			
+			if (motorista == null && veiculo == null) {
+				motorista = new Motorista();
+				veiculo = new Veiculo();
+				motorista.setNome(row.getCell(FileConstants.MOTORISTA_NOME).getStringCellValue());
+				motorista.setCnhNum(sheet.getSheetName());
+				veiculo.setPlaca(row.getCell(FileConstants.PLACA).getStringCellValue());
+				System.out.println(
+						"----- MOTORISTA: " + motorista.getNome() + "Veiculo: " + veiculo.getPlaca() + "-----");
+			}
+
+			Pacote pacote = new Pacote();
+			try {
+				pacote.setDataInsercao(
+						fileDateFormat.parse(row.getCell(FileConstants.DATA_INSERCAO).getStringCellValue()));
+			} catch (ParseException e) {
+				System.err.println(e.getMessage());
+			}
+
+			pacote.setCodLocalizador(row.getCell(FileConstants.RASTREIO).getStringCellValue());
+			pacote.setNomeRemetente(row.getCell(FileConstants.NOME_REMETENTE).getStringCellValue());
+			pacote.setNomeDestino(row.getCell(FileConstants.NOME_DESTINO).getStringCellValue());
+			pacote.setEndRemetente(row.getCell(FileConstants.ENDERECO_REMETENTE).getStringCellValue());
+			pacote.setEndDestino(row.getCell(FileConstants.ENDERECO_DESTINO).getStringCellValue());
+			pacote.setPeso(row.getCell(FileConstants.PESO).getNumericCellValue());
+			pacote.setEntrega(
+					row.getCell(FileConstants.STATUS_ENTREGA).getStringCellValue().equals("sim") ? true : false);
+
+			System.out.println(
+					"Data de inserção: " + fileDateFormat.format(pacote.getDataInsercao()) + " rastreio: "+ pacote.getCodLocalizador() + " Endereço de entrega: "+ pacote.getNomeDestino() + "status de entrega: " + (pacote.isEntrega()? "sim": "nao")  +  "\n");
+
+		}
+	}
 }
