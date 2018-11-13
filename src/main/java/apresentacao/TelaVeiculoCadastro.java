413772f800f98c2934ce916e189938214761c00b
Carlos <Carlos@DESKTOP-E2LLVT3>
2018-11-13 21:39:08 -0200

    Tela Veiculo pronta

--- src/main/java/apresentacao/TelaVeiculoCadastro.java
+++ src/main/java/apresentacao/TelaVeiculoCadastro.java
@@ -7,6 +7,8 @@ import javax.swing.JInternalFrame;
 import javax.swing.JDesktopPane;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+
 import java.awt.Font;
 
 import javax.swing.ImageIcon;
@@ -14,6 +16,8 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class TelaVeiculoCadastro extends JInternalFrame {
 	private JTextField txtPesquisar;
@@ -21,6 +25,7 @@ public class TelaVeiculoCadastro extends JInternalFrame {
 	private JTextField txtModelo;
 	private JTextField txtPlaca;
 	private JTextField txtAno;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -53,87 +58,122 @@ public class TelaVeiculoCadastro extends JInternalFrame {
 		getContentPane().add(desktop);
 		
 		txtPesquisar = new JTextField();
-		txtPesquisar.setBounds(188, 57, 356, 28);
+		txtPesquisar.setBounds(154, 25, 410, 28);
 		desktop.add(txtPesquisar);
 		txtPesquisar.setColumns(10);
 		
-		JLabel lblMarca = new JLabel("Marca:");
-		lblMarca.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblMarca.setBounds(67, 148, 55, 16);
+		JLabel lblMarca = new JLabel("Marca");
+		lblMarca.setForeground(Color.WHITE);
+		lblMarca.setFont(new Font("Calibri", Font.PLAIN, 18));
+		lblMarca.setBounds(80, 218, 55, 16);
 		desktop.add(lblMarca);
 		
-		JLabel lblModelo = new JLabel("Modelo:");
-		lblModelo.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblModelo.setBounds(67, 204, 55, 16);
+		JLabel lblModelo = new JLabel("Modelo");
+		lblModelo.setForeground(Color.WHITE);
+		lblModelo.setFont(new Font("Calibri", Font.PLAIN, 18));
+		lblModelo.setBounds(80, 263, 93, 16);
 		desktop.add(lblModelo);
 		
-		JLabel lblPlaca = new JLabel("Placa:");
-		lblPlaca.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblPlaca.setBounds(67, 249, 55, 16);
+		JLabel lblPlaca = new JLabel("Placa");
+		lblPlaca.setForeground(Color.WHITE);
+		lblPlaca.setFont(new Font("Calibri", Font.PLAIN, 18));
+		lblPlaca.setBounds(80, 308, 55, 16);
 		desktop.add(lblPlaca);
 		
-		JLabel lblAno = new JLabel("Ano:");
-		lblAno.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblAno.setBounds(67, 306, 55, 16);
+		JLabel lblAno = new JLabel("Ano");
+		lblAno.setForeground(Color.WHITE);
+		lblAno.setFont(new Font("Calibri", Font.PLAIN, 18));
+		lblAno.setBounds(80, 353, 55, 16);
 		desktop.add(lblAno);
 		
-		JLabel lblTipo = new JLabel("Tipo:");
-		lblTipo.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblTipo.setBounds(67, 360, 55, 16);
+		JLabel lblTipo = new JLabel("Tipo");
+		lblTipo.setForeground(Color.WHITE);
+		lblTipo.setFont(new Font("Calibri", Font.PLAIN, 18));
+		lblTipo.setBounds(80, 397, 55, 16);
 		desktop.add(lblTipo);
 		
 		
 		
 		
 		txtMarca = new JTextField();
-		txtMarca.setBounds(134, 143, 410, 28);
+		txtMarca.setBounds(154, 212, 410, 28);
 		desktop.add(txtMarca);
 		txtMarca.setColumns(10);
 		
 		txtModelo = new JTextField();
-		txtModelo.setBounds(134, 199, 410, 28);
+		txtModelo.setBounds(154, 257, 410, 28);
 		desktop.add(txtModelo);
 		txtModelo.setColumns(10);
 		
 		txtPlaca = new JTextField();
-		txtPlaca.setBounds(134, 244, 410, 28);
+		txtPlaca.setBounds(154, 302, 410, 28);
 		desktop.add(txtPlaca);
 		txtPlaca.setColumns(10);
 		
 		txtAno = new JTextField();
-		txtAno.setBounds(134, 301, 410, 28);
+		txtAno.setBounds(154, 347, 410, 28);
 		desktop.add(txtAno);
 		txtAno.setColumns(10);
 
 		JButton btnVeiCreate = new JButton("");
 		btnVeiCreate.setToolTipText("Adicionar");
 		btnVeiCreate.setBackground(new Color(214, 217, 223));
-		btnVeiCreate.setIcon(new ImageIcon(TelaMotoristaCadastro.class.getResource("/apresentacao/icones/Add-van.png")));
+		btnVeiCreate.setIcon(new ImageIcon(TelaVeiculoCadastro.class.getResource("/apresentacao/icones/btnVehicle.png")));
 		btnVeiCreate.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnVeiCreate.setBounds(269, 437, 43, 41);
+		btnVeiCreate.setBounds(176, 437, 55, 52);
 		desktop.add(btnVeiCreate);
 		
 		JButton btnVeiUpdate = new JButton("");
 		btnVeiUpdate.setToolTipText("Editar");
-		btnVeiUpdate.setIcon(new ImageIcon(TelaMotoristaCadastro.class.getResource("/apresentacao/icones/edit.png")));
-		btnVeiUpdate.setBounds(338, 437, 43, 41);
+		btnVeiUpdate.setIcon(new ImageIcon(TelaVeiculoCadastro.class.getResource("/apresentacao/icones/btnEdit.png")));
+		btnVeiUpdate.setBounds(274, 437, 55, 52);
 		desktop.add(btnVeiUpdate);		
 
 		JButton btnMotDelete = new JButton("");
 		btnMotDelete.setToolTipText("Deletar");
 		btnMotDelete.setIcon(new ImageIcon(TelaMotoristaCadastro.class.getResource("/apresentacao/icones/delete.png")));
-		btnMotDelete.setBounds(406, 437, 43, 41);
+		btnMotDelete.setBounds(373, 437, 55, 52);
 		desktop.add(btnMotDelete);
 		
 		JComboBox comboBox = new JComboBox();
 		comboBox.addItem("Van");
 		comboBox.addItem("Caminhão");
 		comboBox.addItem("Carreta");
-		comboBox.setBounds(134, 356, 178, 26);
+		comboBox.setBounds(154, 392, 178, 26);
 		desktop.add(comboBox);
+		
+		JButton btnClear = new JButton("");
+		btnClear.setIcon(new ImageIcon(TelaVeiculoCadastro.class.getResource("/apresentacao/icones/btnClear.png")));
+		btnClear.setBounds(473, 437, 55, 52);
+		desktop.add(btnClear);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setViewportBorder(null);
+		scrollPane.setEnabled(false);
+		desktop.setLayer(scrollPane, 0);
+		scrollPane.setBounds(49, 71, 635, 119);
+		desktop.add(scrollPane);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Marca", "Modelo", "Placa", "Ano", "Tipo"
+			}
+		));
+		table.getColumnModel().getColumn(0).setPreferredWidth(59);
+		table.getColumnModel().getColumn(2).setPreferredWidth(56);
+		table.getColumnModel().getColumn(3).setPreferredWidth(49);
+		scrollPane.setViewportView(table);
+		
+		JButton btnPesquisar = new JButton("");
+		btnPesquisar.setIcon(new ImageIcon(TelaVeiculoCadastro.class.getResource("/apresentacao/icones/search-icon.png")));
+		btnPesquisar.setBounds(576, 25, 24, 24);
+		desktop.add(btnPesquisar);
 
 
 	}
